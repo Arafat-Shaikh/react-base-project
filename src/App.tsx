06e07eba2cb9ac3f3219ca20821cadaa7eb8cfd9
Arafat-Shaikh <126144264+Arafat-Shaikh@users.noreply.github.com>
2025-02-07 20:01:14 +0530

    Update App.tsx
    App.tsx modified, one comment added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// This is file contains boiler plate code
+
 function App() {
   const [count, setCount] = useState(0)
 
